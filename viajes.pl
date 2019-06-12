1813155ccb46bff267a5ed5b17a3ccb2d0192a0c
hernanperalta <hernanperalta3@gmail.com>
2019-06-12 18:20:39 -0300

    agrego viajo/2 para explicar not

--- viajes.pl
+++ viajes.pl
@@ -1,27 +1,31 @@
 % punto 1)
 idioma(mexico,          espaniol).
-idioma(brasil,          portugues).
-idioma(eeuu,            ingles).
+idioma(espania,         espaniol).
+idioma(dinamarca,       danes).
 idioma(canada,          ingles).
 idioma(canada,          frances).
-idioma(guayanaFrancesa, frances).
 
 % punto 2)
-sabeHablar(alf, espaniol).
-sabeHablar(alf, ingles).
-sabeHablar(debi, espaniol).
-sabeHablar(debi, portugues).
-sabeHablar(mari, espaniol).
-sabeHablar(mari, frances).
-sabeHablar(mari, aleman).
+sabe(alf, espaniol).
+sabe(alf, ingles).
+sabe(mari, espaniol).
+sabe(mari, frances).
+sabe(mari, danes).
+sabe(debi, espaniol).
+sabe(debi, aleman).
 
 % punto 3)
 empleadeDelMes(mari).
 empleadeDelMes(alf).
 
 % punto 4)
-% punto 5) consulta existencial sobre el predicado puedeViajar/2
+viajo(alf, mexico).
+viajo(mari, espania).
+
+% punto 5)
+% punto 6) consulta existencial sobre el predicado puedeViajar/2
 puedeViajar(Empleade, Pais) :-
     empleadeDelMes(Empleade),
     idioma(Pais, Idioma),
-    sabeHablar(Empleade, Idioma).
+    sabe(Empleade, Idioma),
+    not(viajo(Empleade, Pais)).
